da8520f8f8ec7884ebb4022e5a0f0e3dbea3f286
howhow <debby8698@gmail.com>
2023-09-15 16:39:24 +0800

    refactor: use class refactor app.js

--- app.js
+++ app.js
@@ -6,91 +6,103 @@ const { encode } = require('url-encode-decode')
 const fs = require('fs/promises')
 const dayjs = require('dayjs')
 
-const servicePattern = ''
-const servicePattern_Encode = encode(servicePattern)
-const accessToken = process.env.ACCESS_TOKEN
-
-// 發請求獲取服務資訊
-async function Request(url) {
-  try {
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Basic ${accessToken}`,
-      }})
-
-    return response.json()
-  } catch (err) {
-    console.log(err)
+class CensysAPI {
+  constructor(accessToken) {
+    this.accessToken = accessToken
   }
-}
 
-// 重組自己想獲取的特徵
-async function getData() {
-  try {
-    const url = `https://search.censys.io/api/v2/hosts/search?q=${servicePattern_Encode}&per_page=100`
-    const firResponse = await Request(url)
-    const dataObject = {}
+  // 發請求獲取服務資訊
+  async request(url) {
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${this.accessToken}`,
+        }})
 
-    dataObject.total = firResponse.result.total
-    dataObject.data = firResponse.result.hits.map(data => ({
-      ip: data.ip,
-      country: data.location.country,
-      services: data.services
-    }))
-    
-    let nextLink = firResponse.result.links.next
-    if (!nextLink) return dataObject
+      return response.json()
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
-    while(true) {
-      const nextUrl = `https://search.censys.io/api/v2/hosts/search?q=${servicePattern_Encode}&per_page=100&cursor=${nextLink}`
-      const response = await Request(nextUrl)
+  // 重組自己想獲取的特徵
+  async search(servicePattern_Encode) {
+    try {
+      const url = `https://search.censys.io/api/v2/hosts/search?q=${servicePattern_Encode}&per_page=100`
+      const firResponse = await this.request(url)
+      const dataObject = {}
 
-      response.result.hits.forEach(data => {
-        dataObject.data.push({
-          ip: data.ip,
-          country: data.location.country,
-          services: data.services
+      dataObject.total = firResponse.result.total
+      dataObject.data = firResponse.result.hits.map(data => ({
+        ip: data.ip,
+        country: data.location.country,
+        services: data.services
+      }))
+      
+      let nextLink = firResponse.result.links.next
+      if (!nextLink) return dataObject
+
+      while(true) {
+        const nextUrl = `https://search.censys.io/api/v2/hosts/search?q=${servicePattern_Encode}&per_page=100&cursor=${nextLink}`
+        const response = await this.request(nextUrl)
+
+        response.result.hits.forEach(data => {
+          dataObject.data.push({
+            ip: data.ip,
+            country: data.location.country,
+            services: data.services
+          })
         })
+
+        nextLink = response.result.links.next
+        if (!nextLink) return dataObject
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  // 分類統計國別
+  countryStatistic(dataObject) {
+    try {
+      const countries = []
+      dataObject.data.forEach(data => {
+        if (!countries.find(({ value }, index) => {
+          if (value === data.country) {
+            countries[index].count += 1
+            return true
+          }
+        }) ) countries.push({ "count": 1, "value": data.country })
       })
 
-      nextLink = response.result.links.next
-      if (!nextLink) return dataObject
+      return countries
+    } catch (err) {
+      console.log(err)
     }
-  } catch (err) {
-    console.log(err)
+  }
+
+  // 輸出檔案
+  async writeFile(servicePattern_Encode) {
+    try {
+      const dataObject = await this.search(servicePattern_Encode)
+      const newDataObject = {
+        total: dataObject.total,
+        country: this.countryStatistic(dataObject),
+        data: dataObject.data
+      }
+
+      await fs.appendFile(`./files/${dayjs().format('YYYY-MM-DD')}.json`, JSON.stringify(newDataObject))
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
 
-// 分類統計國別
-function countryStatistic(dataObject) {
-  const countryOjbect = {}
-  dataObject.data.forEach(data => {
-    if (countryOjbect[`${data.country}`]) {
-      countryOjbect[`${data.country}`] += 1
-    } else {
-      countryOjbect[`${data.country}`] = 1
-    }
-  })
+const accessToken = process.env.ACCESS_TOKEN
+const censysAPI = new CensysAPI(accessToken)
 
-  return countryOjbect
-}
-
-// 輸出檔案
-async function writeFile() {
-  try {
-    const dataObject = await getData()
-    const newDataObject = {
-      total: dataObject.total,
-      countryStatistic: countryStatistic(dataObject),
-      data: dataObject.data
-    }
-
-    await fs.appendFile(`./files/${dayjs().format('YYYY-MM-DD')}.json`, JSON.stringify(newDataObject))
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-writeFile()
+const servicePattern = ''
+const servicePattern_Encode = encode(servicePattern)
+censysAPI.writeFile(servicePattern_Encode)
